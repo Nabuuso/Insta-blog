88b3b2632c40095e51fb60325ded3c0c397b4af9
Nabuuso <wagabaliz@gmail.com>
2021-12-07 08:50:04 +0300

    Database configuration

--- instablog/instablog/settings.py
+++ instablog/instablog/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'instablog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': '127.0.0.1',
+        'USER':'nabuusu',
+        'PORT':'5432',
+        'NAME':'instablog',
+        'PASSWORD':'12345'
     }
 }
 
